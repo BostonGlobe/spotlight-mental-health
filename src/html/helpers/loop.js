2513d86f7ed274c435e543549306151d785815b1
michaelworkman <michael.workman@globe.com>
2017-03-09 14:51:54 -0500

    graphic complete

---
+++ src/html/helpers/loop.js
@@ -0,0 +1,13 @@
+module.exports = function(from, to, inc, block) {
+    block = block || {fn: function () { return arguments[0] }}
+
+    var data = block.data || {index: null}
+
+    var output = ''
+    for (var i = from; i <= to; i += inc) {
+        data['index'] = i
+        output += block.fn(i, {data: data})
+    }
+
+    return output
+}
